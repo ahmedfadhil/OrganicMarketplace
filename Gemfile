1b751d97f9c07ad9cf2574f388eece736ff0190d
ahmedfadhil <ahmed.fadhil@alumni.emu.edu.tr>
2016-02-18 21:59:54 +0100

    figaro gem added

--- Gemfile
+++ Gemfile
@@ -23,6 +23,10 @@ gem 'jquery-rails'
 gem 'turbolinks'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
 gem 'jbuilder', '~> 2.0'
+# paper clip dropbox gem
+gem "paperclip-dropbox", ">= 1.1.7"
+# figaro
+gem "figaro"
 # bundle exec rake doc:rails generates the API under doc/api.
 gem 'sdoc', '~> 0.4.0', group: :doc
 
--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,4 +1,7 @@
 class Listing < ActiveRecord::Base
   has_attached_file :image, styles: {large: "500x", medium: "200x", thumb: "100x100>"}, default_url: "default.png"
-  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/,
+                                    :storage => :dropbox,
+                                    :dropbox_credentials => Rails.root.join("config/dropbox.yml")
+
 end
